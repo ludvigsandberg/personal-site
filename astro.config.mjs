9ccef7719f56a075381e05434e43b444d28790c3
Ludvig Sandberg <ludvigss@outlook.com>
2026-01-13 16:56:10 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    base: '/',
+    site: 'https://ludvigsandberg.com',
     integrations: [mdx(), sitemap()],
 
     vite: {
